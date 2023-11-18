16fe0e54250702adbd680d58cd045c3c30f0c3aa
mehrdad Osouli Saran <mehrdadosoulisaran@gmail.com>
2023-11-18 21:40:17 +0330

    get picture from response fetch and send formdata

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -3,6 +3,8 @@ import { useNavigate } from 'react-router-dom';
 import { auth } from './firebase.js';
 import { ChatEngine } from 'react-chat-engine';
 import { AuthContext } from '../contexts/AuthContextProvider.jsx'
+import axios from 'axios';
+
 const Chats = () => {
     const user=useContext(AuthContext)
     const history=useNavigate()
@@ -17,20 +19,39 @@ const Chats = () => {
         return;
         }
         axios.get('https://api.chatengine.io/users/me',{
-            Headers:{
-                'project-id':'',
-                'user-name':user.name,
-                'user-id':user.uid
+            headers:{
+                'project-id':'bd9df0ce-091a-4c98-a9a1-da91ec367fa3',
+                'user-name':user.email,
+                'user-secret':user.uid
             }
         })
         .then(()=>{
             setLoading(false)
         })
         .catch(()=>{
-            let formData=new FormData()
+            let formdata=new FormData()
+            formdata.append('email',user.email)
+            formdata.append('username',user.email)
+            formdata.append('secret',user.uid)
+            getFile(user.photoURL)
+            .then(avatar=>{
+                formdata.append('avatar',avatar,avatar.name)
+                axios.post('https://api.chatengine.io/users/',formdata,{
+                    headers:{
+                        'private-key':'1569c70a-b195-4e18-9657-36f7c33aa0b2'
+                    }
+                }) 
+                .then(()=>setLoading(false))
+                .catch(error=>console.log(error))
+            })
         })
     },[user,loading])
 
+    const getFile=async(url)=>{
+        let response=await fetch(url)
+        let data=await response.blob()
+        return new File([data],'image.jpg',{type:'image/jpeg'})
+    }
     if(!user || loading) return 'loading...'
     return (
         <div>
