1292e1669beaee88a7cef3d9b682a5f0f167c5c5
mehrdadosouli <126550362+mehrdadosouli@users.noreply.github.com>
2023-11-18 10:55:46 +0330

    Chats.jsx

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import React,{useEffect,useState,useContext} from 'react';
 import { useNavigate } from 'react-router-dom';
 import { auth } from './firebase.js';
 import { ChatEngine } from 'react-chat-engine';
+import { AuthContext } from '../contexts/AuthContextProvider.jsx'
 const Chats = () => {
+    const user=useContext(AuthContext)
     const history=useNavigate()
+    const [loading,setLoading]=[true]
+
     const LogOut=async()=>{
         await auth.signOut()
         history('/')
     }
+    useEffect(()=>{
+        if(!user){ history('/')
+        return;
+        }
+        axios.get('https://api.chatengine.io/users/me',{
+            Headers:{
+                'project-id':'',
+                'user-name':user.name,
+                'user-id':user.uid
+            }
+        })
+        .then(()=>{
+            setLoading(false)
+        })
+        .catch(()=>{
+            let formData=new FormData()
+        })
+    },[user,loading])
+
+    if(!user || loading) return 'loading...'
     return (
         <div>
             
